3914929d014787ed8737f4eef0d3665fec724c54
vilyLei <vily313@126.com>
2022-07-25 22:49:30 +0800

    fix: adjust the mutex lock demo.

--- src/test/sliceRaceLock01.go
+++ src/test/sliceRaceLock01.go
@@ -10,28 +10,7 @@ import (
 
 var lock sync.Mutex
 
-func main() {
-
-	fmt.Println("random .................. begin ...")
-	a := rand.Int()
-	b := rand.Intn(100) //生成0-99之间的随机数
-	fmt.Println(a)
-	fmt.Println(b)
-
-	// 将时间戳设置成种子数
-	rand.Seed(time.Now().UnixNano())
-	// 生成10个0-99之间的随机数
-	for i := 0; i < 10; i++ {
-		fmt.Println(rand.Intn(100), ", ", rand.Float32())
-
-	}
-	fmt.Println("random .................. end ...")
-
-	fmt.Println("bytes to Uint32 .................. begin ...")
-	var mySlice = []byte{0, 0, 1, 244}
-	data := binary.BigEndian.Uint32(mySlice)
-	fmt.Println(data)
-	fmt.Println("bytes to Uint32 .................. end ...")
+func test01() {
 
 	slc := make([]int, 0, 1000)
 	var wg sync.WaitGroup
@@ -53,5 +32,53 @@ func main() {
 
 	}
 	wg.Wait()
-	fmt.Println(len(slc))
+	tot := len(slc)
+	fmt.Println("test01(), tot: ", tot)
+}
+
+func test02() {
+
+	tot := 0
+	var wg sync.WaitGroup
+	// var lock sync.Mutex
+
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func(a int) {
+			defer wg.Done()
+
+			// lock.Lock()
+			tot += 1
+			// lock.Unlock()
+		}(i)
+
+	}
+	wg.Wait()
+
+	fmt.Println("test02(), tot: ", tot)
+}
+func main() {
+
+	fmt.Println("random .................. begin ...")
+	a := rand.Int()
+	b := rand.Intn(100) //生成0-99之间的随机数
+	fmt.Println(a)
+	fmt.Println(b)
+
+	// 将时间戳设置成种子数
+	rand.Seed(time.Now().UnixNano())
+	// 生成10个0-99之间的随机数
+	for i := 0; i < 10; i++ {
+		fmt.Println(rand.Intn(100), ", ", rand.Float32())
+	}
+	fmt.Println("random .................. end ...")
+
+	fmt.Println("bytes to Uint32 .................. begin ...")
+	var mySlice = []byte{0, 0, 1, 244}
+	data := binary.BigEndian.Uint32(mySlice)
+	fmt.Println(data)
+	fmt.Println("bytes to Uint32 .................. end ...")
+
+	// test01()
+	test02()
 }
