32f0b5ecae941c1830cd21d52bc93f1aa621bbec
vilyLei <vily313@126.com>
2022-10-28 08:11:56 +0800

    fix: update normalViewer.js code file.

--- bin/static/voxweb3d/demos/normalViewer_debug.js
+++ bin/static/voxweb3d/demos/normalViewer_debug.js
@@ -300,7 +300,7 @@ class DemoTransEditor {
         this.m_vcoapp = new ViewerCoSApp_1.ViewerCoSApp();
         this.m_vcoapp.initialize(() => {
           this.initModel();
-        }, urlChecker);
+        }, urlChecker, true);
       }
     }, urlChecker).addLoader(uiInteractML).load(url0).load(url1);
     uiInteractML.load(url);
@@ -3333,9 +3333,16 @@ class NormalEntityGroup {
 
   loadModels(urls, typeNS = "") {
     if (urls != null && urls.length > 0) {
-      for (let i = 0; i < urls.length; ++i) {
-        this.loadModel(urls[i], typeNS);
-      }
+      // this.m_transforms = [];
+      // this.m_transes = [];
+      let purls = urls.slice(0);
+      this.m_vcoapp.deferredInit(() => {
+        console.log("XXXXXXXXXXXXXXXXXXX deferredInit() call...");
+
+        for (let i = 0; i < purls.length; ++i) {
+          this.loadModel(purls[i], typeNS);
+        }
+      });
     }
   }
 
@@ -3381,27 +3388,47 @@ class NormalEntityGroup {
   }
 
   loadGeomModel(url, format) {
-    let ins = this.m_vcoapp.coappIns;
+    // let ins = this.m_vcoapp.coappIns;
+    let ins = this.m_vcoapp;
 
     if (ins != null) {
       this.uiscene.prompt.getPromptPanel().applyConfirmButton();
       this.uiscene.prompt.showPrompt("Model loading!");
-      this.m_loadTotal++;
+      this.m_loadTotal++; // let unit = ins.getCPUDataByUrlAndCallback(
+      // 	url,
+      // 	format,
+      // 	(unit: CoGeomDataUnit, status: number): void => {
+      // 		if(format != CoDataFormat.FBX) {
+      // 			this.createEntityFromModels(unit.data.models, unit.data.transforms);
+      // 		}
+      // 		this.createEntityFromUnit(unit, status);
+      // 	},
+      // 	true
+      // );
+
       let unit = ins.getCPUDataByUrlAndCallback(url, format, (unit, status) => {
+        if (format != CoSpaceAppData_1.CoDataFormat.FBX) {
+          this.createEntityFromModels(unit.data.models, unit.data.transforms);
+        }
+
         this.createEntityFromUnit(unit, status);
       }, true);
+
+      if (format == CoSpaceAppData_1.CoDataFormat.FBX) {
+        unit.data.modelReceiver = (models, transforms, index, total) => {
+          this.createEntityFromModels(models, transforms);
+        };
+      }
     }
   }
 
-  createEntityFromUnit(unit, status = 0) {
-    console.log("XXXXXX createEntityFromUnit, unit: ", unit);
+  createEntityFromModels(models, transforms) {
     let entities = [];
-    let len = unit.data.models.length;
+    let len = models.length;
     let nodes = [];
 
     for (let i = 0; i < len; ++i) {
-      let dt = unit.data;
-      const node = this.addEntityWithModel(dt.models[i], dt.transforms != null ? dt.transforms[i] : null);
+      const node = this.addEntityWithModel(models[i], transforms != null ? transforms[i] : null);
 
       if (node != null) {
         this.entityManager.addNode(node);
@@ -3413,6 +3440,34 @@ class NormalEntityGroup {
 
     this.updateLayout(false);
     this.transUI.getRecoder().save(entities);
+  }
+
+  createEntityFromUnit(unit, status = 0) {
+    console.log("XXXXXX createEntityFromUnit, unit: ", unit);
+    /*
+    let entities: ITransformEntity[] = [];
+    let len = unit.data.models.length;
+      let nodes: NormalEntityNode[] = [];
+    for (let i = 0; i < len; ++i) {
+        let dt = unit.data;
+        const node = this.addEntityWithModel(dt.models[i], dt.transforms != null ? dt.transforms[i] : null);
+        if (node != null) {
+            this.entityManager.addNode(node);
+            nodes.push(node);
+            this.m_nodes.push(node);
+            entities.push(node.entity);
+        }
+    }
+    
+    this.updateLayout(false);
+    this.transUI.getRecoder().save(entities);
+    
+    // for (let i = 0; i < nodes.length; ++i) {
+    // 	nodes[i].createNormalLine();
+    // }
+    //*/
+
+    let nodes = this.m_nodes;
 
     for (let i = 0; i < nodes.length; ++i) {
       nodes[i].createNormalLine();
@@ -3450,10 +3505,17 @@ class NormalEntityGroup {
     }
 
     this.m_layoutor.rotationEnabled = rotationEnabled;
+
+    for (let k = 0; k < this.m_transes.length; ++k) {
+      let et = this.m_transes[k];
+      et.setXYZ(0.0, 0.0, 0.0);
+      et.setScaleXYZ(1.0, 1.0, 1.0);
+      et.setRotationXYZ(0.0, 0.0, 0.0);
+    }
+
     let pivot = CoRScene.createVec3();
-    this.m_layoutor.fixToPosition(this.m_transes, this.m_transforms, pivot, 300.0);
-    this.m_transforms = null;
-    this.m_transes = null;
+    this.m_layoutor.fixToPosition(this.m_transes, this.m_transforms, pivot, 300.0); // this.m_transforms = null;
+    // this.m_transes = null;
   }
 
   destroy() {
@@ -5608,9 +5670,16 @@ const CoSpaceAppData_1 = __webpack_require__("3347");
 const ModuleLoader_1 = __webpack_require__("75f5");
 
 class ViewerCoSApp {
-  constructor() {}
+  constructor() {
+    this.m_urlChecker = null;
+    this.m_initInsFlag = true;
+    this.m_initCalls = [];
+    this.m_flag = true;
+  }
 
-  initialize(callback, urlChecker = null) {
+  initialize(callback, urlChecker = null, deferredInit = false) {
+    this.m_deferredInit = deferredInit;
+    this.m_urlChecker = urlChecker;
     let modules = [{
       url: "static/cospace/core/coapp/CoSpaceApp.umd.js",
       name: CoSpaceAppData_1.CoModuleNS.coSpaceApp,
@@ -5636,31 +5705,120 @@ class ViewerCoSApp {
       name: CoSpaceAppData_1.CoModuleNS.fbxFastParser,
       type: CoSpaceAppData_1.CoModuleFileType.JS
     }];
-    this.m_modules = modules;
+    this.m_modules = modules; // 初始化数据协同中心
+
+    let dependencyGraphObj = {
+      nodes: [{
+        uniqueName: "dracoGeomParser",
+        path: "static/cospace/modules/draco/ModuleDracoGeomParser.umd.js"
+      }, {
+        uniqueName: "dracoWasmWrapper",
+        path: "static/cospace/modules/dracoLib/w2.js"
+      }, {
+        uniqueName: "ctmGeomParser",
+        path: "static/cospace/modules/ctm/ModuleCTMGeomParser.umd.js"
+      }],
+      maps: [{
+        uniqueName: "dracoGeomParser",
+        includes: [1]
+      } // 这里[1]表示 dracoGeomParser 依赖数组中的第一个元素也就是 dracoWasmWrapper 这个代码模块
+      ]
+    };
+    this.m_dependencyGraphObj = dependencyGraphObj;
 
     if (urlChecker != null) {
       for (let i = 0; i < modules.length; ++i) {
         modules[i].url = urlChecker(modules[i].url);
       }
+
+      let nodes = dependencyGraphObj.nodes;
+
+      for (let i = 0; i < nodes.length; ++i) {
+        nodes[i].path = urlChecker(nodes[i].path);
+      }
     }
 
-    new ModuleLoader_1.ModuleLoader(1, null, urlChecker).setCallback(() => {
-      this.initCoSpaceApp();
+    if (!deferredInit) {
+      new ModuleLoader_1.ModuleLoader(1, null, urlChecker).setCallback(() => {
+        this.initCoSpaceApp();
 
-      if (callback != null) {
-        callback();
+        if (callback != null) {
+          callback();
+        }
+      }).load(this.m_modules[0].url);
+    }
+  }
+  /**
+   * 注意: 不建议过多使用这个函数,因为回调函数不安全如果是lambda表达式则由性能问题。
+   * 立即获得CPU侧的数据单元实例, 但是数据单元中的数据可能是空的, 因为数据获取的操作实际是异步的。
+   * 需要通过 isCpuPhase() 或者 isGpuPhase() 等函数来判定具体数据情况
+   * @param url 数据资源url
+   * @param dataFormat 数据资源类型
+   * @param callback 数据资源接收回调函数, 其值建议为lambda函数表达式
+   * @param immediate 是否立即返回数据, 默认是false
+   * @returns 数据单元实例，用户只能访问不能更改这个实例内部的数据状态，如果必要则可以申请复制一份
+   */
+
+
+  getCPUDataByUrlAndCallback(url, dataFormat, callback, immediate) {
+    if (this.coappIns != null) {
+      let unit = this.coappIns.getCPUDataByUrlAndCallback(url, dataFormat, callback, immediate);
+
+      if (this.m_deferredInit) {
+        if (this.m_initInsFlag) {
+          this.m_initInsFlag = false;
+          let modules = this.m_modules;
+          this.coappIns.initialize(3, modules[1].url, true);
+        }
       }
-    }).load(this.m_modules[0].url);
+
+      return unit;
+    }
+
+    return null;
+  }
+
+  deferredInit(callback) {
+    if (this.coappIns == null) {
+      this.m_initCalls.push(callback);
+
+      if (this.m_flag) {
+        new ModuleLoader_1.ModuleLoader(1, null, this.m_urlChecker).setCallback(() => {
+          this.initCoSpaceApp();
+
+          if (callback != null) {
+            callback();
+          }
+        }).load(this.m_modules[0].url);
+        this.m_flag = false;
+      }
+    } else if (callback != null) {
+      callback();
+    }
   }
 
   initCoSpaceApp() {
     if (this.coappIns == null && typeof CoSpaceApp !== "undefined") {
       let coappIns = CoSpaceApp.createInstance();
       let modules = this.m_modules;
+      let jsonStr = JSON.stringify(this.m_dependencyGraphObj);
+      coappIns.setThreadDependencyGraphJsonString(jsonStr);
       coappIns.setTaskModuleParams(modules);
-      coappIns.initialize(3, modules[1].url, true);
+
+      if (!this.m_deferredInit) {
+        coappIns.initialize(3, modules[1].url, true);
+      }
+
       let t = this;
       t.coappIns = coappIns;
+
+      for (let i = 0; i < this.m_initCalls.length; ++i) {
+        if (this.m_initCalls[i] != null) {
+          this.m_initCalls[i]();
+        }
+      }
+
+      this.m_initCalls = [];
     }
   }
 
